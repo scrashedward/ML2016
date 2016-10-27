7ba7d71d9396d835520dacbe27a81b24673f1039
scrashedward <scrashedward@gmail.com>
2016-10-27 15:37:39 +0800

    Done

--- hw2/nntrain.py
+++ hw2/nntrain.py
@@ -3,9 +3,11 @@
 import pandas as pd
 import numpy as np
 from time import gmtime, strftime
-#from sklearn.preprocessing import normalize
 from random import random, randint
 
+print strftime("%Y-%m-%d %H:%M:%S", gmtime())
+
+print 'Program finish after 25 models trained'
 
 eta = 0.00000001
 
@@ -16,7 +18,6 @@ def error(o,y):
 	return (-(y*np.log(o+eta) + (1-y)*np.log(1-o+eta))).mean()
 
 
-print strftime("%Y-%m-%d %H:%M:%S", gmtime())
 #read data from csv
 data = pd.read_csv('spam_train.csv', index_col = 0, header = None)
 
@@ -25,30 +26,17 @@ data = pd.read_csv('spam_train.csv', index_col = 0, header = None)
 temp = np.zeros(4001) + 1
 train_data = data.ix[1:, :57].as_matrix()
 #train_data = normalize(train_data, axis = 0)
-train_data = train_data / (np.linalg.norm(train_data, ord = 2, axis = 0))
+#train_data = train_data / (np.linalg.norm(train_data, ord = 2, axis = 0))
+train_data = (train_data - train_data.mean(axis = 0))/train_data.std(axis = 0)
 train_data = np.column_stack((train_data, temp.T))
-
-
-#self_test = data.ix[3802:, :57].as_matrix()
-#self_test = normalize(self_test, axis = 0)
 
 #y is the result ndarray of size 4001, 1
 y = data.ix[1:, 58].as_matrix()
-self_y = data.ix[3802:, 58].as_matrix()
 
-test_data = pd.read_csv('spam_test.csv',index_col = 0, header = None)
-test_data = test_data.ix[1:,:].as_matrix()
-#test_data = normalize(test_data, axis = 0)
-test_data = test_data / (np.linalg.norm(test_data, ord = 2, axis = 0))
-test_data = np.column_stack((test_data, np.ones(600)))
-
-print data.ix[1:,:].as_matrix().shape
-print train_data.shape
-
-adbNum = 31
+model = open('model', 'w+')
+adbNum = 25
 w_input = []
 w = []
-self_test_result = np.zeros(200)
 test_result = np.zeros(600)
 for i in range(adbNum):
 
@@ -86,50 +74,9 @@ for i in range(adbNum):
 		#	print roundNum
 			o_input = logistic(train_data.dot(w_input[i].T))
 			o = logistic(np.column_stack((o_input, np.ones(dataLen))).dot(w[i]))
-			print error(o, y)
+			print str(i) + ": " + str(error(o, y))
 		#	raw_input()
-	
-		
-	test_input = logistic(test_data.dot(w_input[i].T))
-	test_o = logistic(np.column_stack((test_input, np.ones(600))).dot(w[i]))
-	
-	test_o[test_o < 0.5] = 0
-	test_o[test_o >= 0.5] = 1
-	test_result = test_result + test_o
-		
-	'''
-	self_test_stack = np.column_stack((self_test, np.ones(200)))
-	self_input = logistic(self_test_stack.dot(w_input[i].T))
-	self_o = logistic(np.column_stack((self_input, np.ones(200))).dot(w[i]))
-
-	print 'self test error:' + str(error(self_o,self_y))
-	self_o[self_o < 0.5] = 0
-	self_o[self_o >= 0.5] = 1
-	self_test_result = self_test_result + self_o
-	
-	
-print self_test_result
-self_test_result[self_test_result > adbNum/2] = 1
-self_test_result[self_test_result < adbNum/2] = 0
-
-wrong = 0
-for i in range(200):
-	if self_test_result[i] != self_y[i]:
-		wrong = wrong + 1
-
-print 'wrong:' + str(wrong)
-
-'''
-
-test_o[test_result > float(adbNum)/float(2)] = 1
-test_o[test_result < float(adbNum)/float(2)] = 0
-
-#print test_o
-
-out = open('nn.csv','w+')
-out.write('id,label\n')
-for i in range(1,601):
-	out.write(str(i) + "," + str(int(test_o[i-1])) + '\n')
-out.close()
+	np.savetxt(model, w_input[i], delimiter = ',', footer = '=')
+	np.savetxt(model, w[i], delimiter = ',', footer = '=')
 
 print strftime("%Y-%m-%d %H:%M:%S", gmtime())
---
+++ hw2/test.py
@@ -0,0 +1,59 @@
+import numpy as np
+import pandas as pd
+from time import gmtime, strftime
+
+print strftime("%Y-%m-%d %H:%M:%S", gmtime())
+
+eta = 0.00000001
+
+def logistic(z):
+	return 1/(1+np.exp(-z+eta))
+	
+test_data = pd.read_csv('spam_test.csv',index_col = 0, header = None)
+test_data = test_data.ix[1:,:].as_matrix()
+test_data = (test_data - test_data.mean(axis = 0))/test_data.std(axis = 0)
+test_data = np.column_stack((test_data, np.ones(600)))
+
+model = open('model', 'r')
+
+line = model.readline()
+test_result = np.zeros(600)
+adbNum = 0;
+while line != '':
+	adbNum = adbNum + 1
+	lines = []
+	wlines = []
+	while line != '# =\n':
+		lines.append(line)
+		line = model.readline()
+	line = model.readline()
+	while line != '# =\n':
+		wlines.append(line)
+		line = model.readline()
+	line = model.readline()
+	
+	w_input = np.zeros((len(lines),58))
+	w = np.zeros(len(wlines))
+	for i in range(len(lines)):
+		w_input[i] = np.fromstring(lines[i], sep = ',')
+	for i in range(len(wlines)):
+		w[i] = np.fromstring(wlines[i], sep = ',')
+	
+	test_input = logistic(test_data.dot(w_input.T))
+	test_o = logistic(np.column_stack((test_input, np.ones(600))).dot(w))
+	test_o[test_o < 0.5] = 0
+	test_o[test_o >= 0.5] = 1
+	test_result = test_result + test_o
+
+test_o[test_result > float(adbNum)/float(2)] = 1
+test_o[test_result < float(adbNum)/float(2)] = 0
+
+#print test_o
+
+out = open('nn_test.csv','w+')
+out.write('id,label\n')
+for i in range(1,601):
+	out.write(str(i) + "," + str(int(test_o[i-1])) + '\n')
+out.close()
+
+print strftime("%Y-%m-%d %H:%M:%S", gmtime())
